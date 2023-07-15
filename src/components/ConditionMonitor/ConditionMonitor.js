6b586d44a4c306abec76a0b2537c48bebb7651df
Dean Petty <dean.petty@gmail.com>
2023-07-14 21:12:32 -0500

    Checkin now that this is actually working kind of the way i want

--- src/components/ConditionMonitor/ConditionMonitor.js
+++ src/components/ConditionMonitor/ConditionMonitor.js
@@ -1,49 +1,62 @@
+import React, { useState } from 'react';
 import './ConditionMonitor.css';
+
 const ConditionMonitor = (props) => {
-    const styles = {
-        rectangle: {
-            width: '25px',
-            height: '25px',
-            border: 'solid 1px black',
-            display: 'inline-block',
-            margin: '1px',
-            cursor: 'pointer',
-            textAlign: 'center'
-        }
-    }
+  const [selectedCondition, setSelectedCondition] = useState(null);
 
-    const handleClick = (options) => {
-        var wounds = options.target.dataset.number;
-        if(wounds === "0" && options.target.parentElement.children[0].classList.contains('damaged') && !options.target.parentElement.children[1].classList.contains('damaged')){
-            options.target.parentElement.children[0].classList.remove('damaged');
-        }else{
-            var children = options.target.parentElement.children;
-            for(var i=0; i < children.length; i++){
-                if(children[i].dataset.number <= wounds){
-                    children[i].classList.add('damaged');
-                }else{
-                    children[i].classList.remove('damaged');
-                }
-            }
-        }
-    }
+  const handleClick = (number) => {
+    setSelectedCondition((prevCondition) => {
+      if (prevCondition === number) {
+        return null; // Unselect the condition if it was already selected
+      } else {
+        return number; // Select the condition if it was not previously selected
+      }
+    });
+  };
 
-    return (
-        <div className="conditionMonitor">
-            <div className="conditionBoxes"><span>{props.type}</span>
-                <div data-number={0} onClick={handleClick} style={styles.rectangle}><span>L</span></div>
-                <div data-number={1} onClick={handleClick} style={styles.rectangle}><span>&nbsp;</span></div>
-                <div data-number={2} onClick={handleClick} style={styles.rectangle}><span>M</span></div>
-                <div data-number={3} onClick={handleClick} style={styles.rectangle}><span>&nbsp;</span></div>
-                <div data-number={4} onClick={handleClick} style={styles.rectangle}><span>&nbsp;</span></div>
-                <div data-number={5} onClick={handleClick} style={styles.rectangle}><span>S</span></div>
-                <div data-number={6} onClick={handleClick} style={styles.rectangle}><span>&nbsp;</span></div>
-                <div data-number={7} onClick={handleClick} style={styles.rectangle}><span>&nbsp;</span></div>
-                <div data-number={8} onClick={handleClick} style={styles.rectangle}><span>&nbsp;</span></div>
-                <div data-number={9} onClick={handleClick} style={styles.rectangle}><span>D</span></div>
-            </div>
+  const renderBoxes = () => {
+    const boxes = ['L', '_', 'M', '_', '_', 'S', '_', '_', '_', 'D'];
+
+    return boxes.map((box, index) => {
+      const isSelected = selectedCondition !== null && index <= selectedCondition;
+
+      return (
+        <div
+          key={index}
+          data-number={index}
+          onClick={() => handleClick(index)}
+          style={{
+            ...styles.rectangle,
+            borderColor: isSelected ? 'red' : 'black',
+            backgroundColor: isSelected ? 'red' : 'transparent',
+          }}
+        >
+          <span>{box}</span>
         </div>
-    );
+      );
+    });
+  };
 
-}
+  return (
+    <div className="conditionMonitor">
+      <div className="conditionBoxes">
+        <span>{props.type}</span>
+        {renderBoxes()}
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  rectangle: {
+    width: '25px',
+    height: '25px',
+    border: 'solid 1px black',
+    display: 'inline-block',
+    margin: '1px',
+    cursor: 'pointer',
+    textAlign: 'center',
+  },
+};
+
 export default ConditionMonitor;
